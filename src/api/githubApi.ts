9336cbf6ac11c345f4681e381b732f4c49846589
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2023-12-19 10:44:40 -0400

    Update githubApi.ts

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -4,7 +4,7 @@ const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
     Authorization:
-      'Bearer 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',
+     
   },
 });
 
